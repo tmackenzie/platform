60a1683724efd50c33547c6da70191fe63df26bb
= <tom.s.mackenzie@gmail.com>
2022-04-21 09:14:10 -0400

    feat: certs and api

--- cache.tf
+++ cache.tf
@@ -1,7 +1,8 @@
-resource "aws_elasticache_security_group" "cache_sg" {
-  name                 = "cache-sg"
-  security_group_names = [aws_security_group.platform_sg.name]
-}
+# TODO: figure out how to use subnets b/c
+# Error: Error creating CacheSecurityGroup: InvalidParameterValue: Use of cache security groups is not 
+# permitted in this API version for your account.
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_subnet_group
+
 
 
 resource "aws_elasticache_cluster" "cache" {
